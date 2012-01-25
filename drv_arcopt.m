24a41e2480eda56ba54a3a775d8b214b5717f8a9
Nick Henderson <nwh@stanford.edu>
2012-01-25 11:41:04 -0800

    just ran exp 2012-01-25

--- drv_arcopt.m
+++ drv_arcopt.m
@@ -3,19 +3,35 @@
 function drv_arcopt
   
   % choose results dir
-  res_dir = 'results/2012-01-18/';
+  res_dir = 'results/2012-01-25/';
+  run_dir = [res_dir 'runs/'];
   
-  % create results dir if it does not exist
+  % settings
+  solve_tol = -.99;
+  
+  % create dirs if they don't exist
   if ~exist(res_dir,'dir')
     mkdir(res_dir);
+  end
+  if ~exist(run_dir,'dir')
+    mkdir(run_dir);
   end
   
   % load data
   D = load('graphs/hcp_data.mat');
   
+  % get lengths of different data sets
+  len_set = length(D.hcp_set);
+  len_10 = length(D.hcp_10);
+  len_12 = length(D.hcp_12);
+  len_14 = length(D.hcp_14);
+  
   % combine data
-  hcp_data = [D.hcp_set; D.hcp_10; D.hcp_12; D.hcp_14 ];
+  hcp_data = [D.hcp_set; D.hcp_10; D.hcp_12; D.hcp_14];
+  hcp_code = [ones(len_set,1); 2*ones(len_10,1); 
+    3*ones(len_12,1); 4*ones(len_14,1)];
   hcp_num = length(hcp_data);
+  %hcp_num = 10;
   
   % attempt to solve
   res_arcopt = [];
@@ -23,13 +39,20 @@ function drv_arcopt
   for i = 1:hcp_num
     fprintf('working on: %13s is_hamil: %d\n',hcp_data(i).name,hcp_data(i).is_hamil);
     if hcp_data(i).is_hamil
-      res = hcp_arcopt(hcp_data(i).P,hcp_data(i).name);
+      run_fname = [run_dir mod_fname(hcp_data(i).name) '.txt'];
+      res = hcp_arcopt(hcp_data(i).P,hcp_data(i).name,solve_tol,run_fname);
       res_arcopt = [res_arcopt; res];
     end
     progressbar(i/hcp_num);
   end
   
   % save results
-  save([res_dir 'res_arcopt.mat'],'res_arcopt')
+  save([res_dir 'res_arcopt.mat'],'res_arcopt','hcp_data','hcp_code');
+  
+end
+
+function s = mod_fname(fname)
+  
+  s = regexprep(fname, '[()]', '_');
   
 end
--- hcp_arcopt.m
+++ hcp_arcopt.m
@@ -1,6 +1,6 @@
 %hcp_arcopt  attempt to solve hcp with arcopt
 
-function [hcp_slv] = hcp_arcopt(P,name,solve_tol)
+function [hcp_slv] = hcp_arcopt(P,name,solve_tol,print_fname)
   
   % handle optional input
   if nargin < 2 || isempty(name)
@@ -9,6 +9,10 @@ function [hcp_slv] = hcp_arcopt(P,name,solve_tol)
   
   if nargin < 3 || isempty(solve_tol)
     solve_tol = -.99;
+  end
+  
+  if nargin < 4 || isempty(print_fname)
+    print_fname = '';
   end
   
   % get number of edges
@@ -33,10 +37,19 @@ function [hcp_slv] = hcp_arcopt(P,name,solve_tol)
   bl = zeros(num_edges,1);
   bu = ones(num_edges,1);
   
+  % open print file if requested
+  if print_fname
+    print_fid = fopen(print_fname,'w');
+  else
+    print_fid = 0;
+  end
+  
   % options for arcopt
   options = arcopt_nm_lc.optset();
   options.crash = 'firstm';
-  options.print_level = 'none';
+  options.print_file = print_fid;
+  options.print_level = 'iter';
+  options.print_screen = 0;
 
   % solve
   mysolver = arcopt_nm_lc(func,hess,x0,bl,bu,A,c,c,options);
@@ -54,6 +67,11 @@ function [hcp_slv] = hcp_arcopt(P,name,solve_tol)
     hcp_slv.hc_found = 1;
   else
     hcp_slv.hc_found = 0;
+  end
+  
+  % close file if opened
+  if print_fname
+    fclose(print_fid);
   end
   
   %keyboard
